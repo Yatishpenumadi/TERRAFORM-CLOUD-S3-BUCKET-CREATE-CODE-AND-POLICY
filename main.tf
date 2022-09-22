5f39fada147591db4b0b55e9f5a495b5452d3e41
Yatishpenumadi <110468458+Yatishpenumadi@users.noreply.github.com>
2022-09-22 16:50:51 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -18,6 +18,26 @@ Name = "MY BUCKET"
 Environment = "Dev"
 }
 }
+resource "aws_s3_bucket_policy" "allow_access_from_public" {
+bucket = aws_s3_bucket.storage-bucket.id
+policy = data.aws_iam_policy_document.allow_access_from_public.json
+}
+data "aws_iam_policy_document" "allow_access_from_public" {
+statement {
+principals {
+type = "AWS"
+identifiers = ["*"]
+}
+actions = [
+"s3:GetObject",
+"s3:ListBucket",
+]
+resources = [
+aws_s3_bucket.storage-bucket.arn,
+"${aws_s3_bucket.storage-bucket.arn}/*",
+]
+}
+}
 
 
 
