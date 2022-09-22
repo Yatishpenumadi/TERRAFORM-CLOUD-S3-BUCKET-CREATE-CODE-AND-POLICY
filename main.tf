20fba9dcb58d79a51f20aede796f4970d302e29c
Yatishpenumadi <110468458+Yatishpenumadi@users.noreply.github.com>
2022-09-22 15:35:10 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -15,15 +15,14 @@ version = "~>3.0"
 }
 
 }
-terraform {
-required_version = "~>1.3.0" 
-required_providers {
  
-source = "hashicorp/aws"
- version = "~>3.0"
+ {
+ 
+
+ 
 region = "ap-south-1"
 
 profile = "default"
  }
-}
+
 
