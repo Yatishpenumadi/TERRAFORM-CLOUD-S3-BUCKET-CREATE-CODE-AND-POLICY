7b503b7535f47a557bafb02de5b743299cc5c4d0
Yatishpenumadi <110468458+Yatishpenumadi@users.noreply.github.com>
2022-09-23 12:12:14 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -7,12 +7,12 @@ version = "~>4.31.0"
 }
 }
 }
- provider =
+ provider {
  aws  {
  region = "ap-south-1"
  profile = "default"
  
-  
+ } 
  }
 resource "aws_s3_bucket" "storage-bucket" {
 bucket = "my-s9-bucket"
