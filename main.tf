7ad23fe6826aacba2a600097d4e61fd66e1f5321
Yatishpenumadi <110468458+Yatishpenumadi@users.noreply.github.com>
2022-09-23 11:49:03 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -3,13 +3,14 @@ terraform {
 required_providers {
 aws = {
 source = "hashicorp/aws"
-version = "~>3.0"
+version = "~>1.0"
 }
 }
 }
  provider "aws" {
  region = "ap-south-1"
  profile = "default"
+  
  }
 resource "aws_s3_bucket" "storage-bucket" {
 bucket = "my-s9-bucket"
