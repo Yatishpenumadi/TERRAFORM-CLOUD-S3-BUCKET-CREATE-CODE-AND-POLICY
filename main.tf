8d7ebdf46c4b8484a5688a69854b91c998508338
Yatishpenumadi <110468458+Yatishpenumadi@users.noreply.github.com>
2022-09-23 17:11:42 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,16 +1,16 @@
 terraform {
-required_version = "~>1.0"
+required_version = "~>1.3.0"
 required_providers {
 aws = {
 source = "hashicorp/aws"
-version = "~>4.31.0"
+version = "~>4.24.0"
 }
 }
 }
  provider "aws"   {
   
 region = "ap-south-1"
- profile = "default"
+
  
  
  }
